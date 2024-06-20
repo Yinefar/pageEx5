7324e96180febd5990612aabf49f20a58c0e2325
Yinefar Yiniz <yinefar.yiniz@gmail.com>
2024-06-20 09:42:31 -0500

    Create script.js
    Date  |   Time   |   Carrousel

---
+++ script.js
@@ -0,0 +1,26 @@
+let fecha=new Date();
+let d=fecha.getDate();
+let m=fecha.getMonth()+1;
+let y=fecha.getFullYear();
+
+document.getElementById("pf").innerHTML="Fecha:"+d+"/"+m+"/"+y;
+function reloj() {
+    let fecha=new Date();
+let h=fecha.getHours();
+let m=fecha.getMinutes();
+let s=fecha.getSeconds();
+
+document.getElementById("ph").innerHTML="Hora:"+h+":"+m+":"+s;
+setTimeout("reloj()", 1000);
+}
+document.body.setAttribute("onload", "reloj()");
+
+var c=0;
+function carrusel() {
+    c++;
+    if(c>5) c=1;
+    document.getElementById("imgcab").setAttribute("src","img/ban"+c+".jpg");
+    setTimeout("carrusel()",1000);
+}
+
+document.body.setAttribute("onload", "reloj(); carrusel()");
